c4661d1b46c28f67bd3f87d916843bab8e596c7a
Lucas Libshutz <lsl94@cornell.edu>
2026-08-31 22:30:51 -0400

    moved files around for easier navigation

---
+++ src/lie/coeffs.rs
@@ -0,0 +1,1 @@
+pub(super) const SMALL_ANGLE: f64 = 1e-8;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use nalgebra::Vector3;
-use rust_inekf::lie::hat;
+use rust_inekf::lie::so3;
 
 fn main() {
     let w = Vector3::new(1.0, 2.0, 3.0);
-    println!("{}", hat(&w));
+    println!("{}", so3::hat(&w));
 }
