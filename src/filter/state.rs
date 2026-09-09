f543c3dc89c225616e2373f293da6ae2f1056e2b
Lucas Libshutz <lsl94@cornell.edu>
2026-09-09 11:49:55 -0400

    state implementation before tests

--- src/filter/state.rs
+++ src/filter/state.rs
@@ -11,5 +11,18 @@ pub struct CarState {
 
 impl CarState {
     // pub fn new(x: SE23, cov: Matrix9) 
-    pub fn from_sigmaw
+    pub fn new(x: SE23, cov: Matrix9) -> Self { Self { x, cov }}
+    pub fn from_sigmas(x: SE23, sigma_phi: Vector3<f64>, sigma_vel: Vector3<f64>, sigma_pos:Vector3<f64>) -> Self {
+        let mut sigmas = Vector9::zeros();
+        sigmas.fixed_rows_mut::<3>(0).copy_from(&sigma_phi);
+        sigmas.fixed_rows_mut::<3>(3).copy_from(&sigma_vel);
+        sigmas.fixed_rows_mut::<3>(6).copy_from(&sigma_pos);
+        Self::new(x, Matrix9::from_diagonal(&sigmas.map(|s| s * s)))
+    }
+    pub fn symmetrize(&mut self) {
+        self.cov = self.cov.symmetric_part();
+    }
+    pub fn is_psd(&self) -> bool {
+        Cholesky::new(self.cov).is_some()
+    }
 }
