53577b6b8c6ad664128ee623cdbac96de8462012
Lucas Libshutz <lsl94@cornell.edu>
2026-09-09 13:50:16 -0400

    added DT transition and test

--- src/filter/propagate.rs
+++ src/filter/propagate.rs
@@ -4,6 +4,8 @@ use crate::lie::so3;
 use super::sensors::IMU;
 use nalgebra::{Matrix3, Vector3};
 
+//NOTE: gamma computed multiple times, should only be done once via data structure and passed down.
+
 pub struct ProcessNoise {
     pub sigma_gyro: f64, // rad/s/sqrt(Hz)
     pub sigma_accel: f64, // (m/s^2)/sqrt(Hz)
@@ -44,4 +46,70 @@ pub fn propagate_mean(x: &SE23, imu: &IMU, dt: f64) -> SE23 {
         x.v + GRAVITY_VECTOR * dt + x.r * (g.g1 * imu.accel) * dt,
         x.p + x.v * dt + 0.5 * GRAVITY_VECTOR * dt.powi(2) + x.r * (g.g2 * imu.accel) * dt.powi(2)
     )
+}
+
+pub fn transition_matrix(imu: &IMU, dt:f64) -> Matrix9 {
+    let phi = imu.gyro * dt;
+    let g = gammas(&phi);
+    let g0t = g.g0.transpose();
+
+    // Body frame velocity & accel
+    let body_vel = g.g1 * imu.accel * dt;
+    let body_pos = g.g2 * imu.accel * dt.powi(2);
+
+    let mut f = Matrix9::zeros();
+    f.fixed_view_mut::<3,3>(0,0).copy_from(&g0t);
+    f.fixed_view_mut::<3,3>(3,3).copy_from(&g0t);
+    f.fixed_view_mut::<3,3>(6,6).copy_from(&g0t);
+    f.fixed_view_mut::<3,3>(3,0).copy_from(&(-g0t * so3::hat(&body_vel)));
+    f.fixed_view_mut::<3,3>(6,0).copy_from(&(-g0t * so3::hat(&body_pos)));
+    f.fixed_view_mut::<3,3>(6,3).copy_from(&(g0t * dt));
+    f
+}
+
+fn discrete_noise(phi_mat: &Matrix9, noise: &ProcessNoise, dt: f64) -> Matrix9 {
+    phi_mat * noise.continuous() * phi_mat.transpose() * dt
+}
+
+impl CarState {
+    pub fn propagate(&mut self, imu: &IMU, noise: &ProcessNoise, dt:f64) {
+        let f = transition_matrix(imu, dt);
+        self.x = propagate_mean(&self.x, imu, dt);
+        self.cov = f * self.cov * f.transpose() + discrete_noise(&f, noise, dt);
+        self.symmetrize();
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use::nalgebra::Vector3;
+    use approx::assert_relative_eq;
+    #[test]
+    fn zoh_composes() {
+        let x0 = SE23::new(
+            so3::exp(&Vector3::new(0.3, -0.7, 1.1)),
+            Vector3::new(2.0,-1.0,0.5),
+            Vector3::new(-3.0,4.0,1.0),
+        );
+        let accel = Vector3::new(1.5,-0.3,-9.0);
+
+        for gyro in [
+            Vector3::zeros(),
+            Vector3::new(1e-12,0.0,0.0),
+            Vector3::new(0.0,0.0,2.0),
+            Vector3::new(0.0, 0.0, 3.33),
+        ] {
+            let imu = IMU { gyro, accel };
+            let dt = 0.06;
+            let one = propagate_mean(&x0, &imu, dt);
+            let mut many = x0;
+            for _ in 0..64 {
+                many = propagate_mean(&many, &imu, dt / 64.0);
+            }
+            assert_relative_eq!(
+                one.to_matrix(), many.to_matrix(), epsilon=1e-10
+            );
+        }
+    }
 }
